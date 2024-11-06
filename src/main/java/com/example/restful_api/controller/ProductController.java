66cb015fad5aad3768e76893f078237ebb5505ba
arcanlte <pinangayjoel@gmail.com>
2024-11-05 20:36:22 -0500

    Third commit

--- src/main/java/com/example/restful_api/controller/ProductController.java
+++ src/main/java/com/example/restful_api/controller/ProductController.java
@@ -1,4 +1,49 @@
 package com.example.restful_api.controller;
 
+import com.example.restful_api.model.Product;
+import com.example.restful_api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    // Get a product by ID
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    // Create a new product
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    // Update an existing product
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        return productService.updateProduct(id, productDetails);
+    }
+
+    // Delete a product
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
 }
--- src/main/java/com/example/restful_api/repository/ProductRepository.java
+++ src/main/java/com/example/restful_api/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.restful_api.repository;
 
-public class ProductRepository {
+import com.example.restful_api.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
